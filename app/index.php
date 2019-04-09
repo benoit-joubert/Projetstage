6acde80c9824abb73fbd13ac8409fa8804657c0a
Kévin-Falco <kevin.falco@etu.univ-amu.fr>
2019-04-09 15:37:14 +0200

    Modification du HTML pour permettre son dynamise en JS

--- app/index.php
+++ app/index.php
@@ -1,28 +1,10 @@
-<?php
-
-?>
 <html lang="FR">
 <head>
     <title>Recommandé</title>
     <link rel="stylesheet" href="style/style.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script rel="script" src="script/header.js"></script>
 </head>
 <body>
-    <div id="menu">
-        <ul id="onglets">
-            <li class="active"><a href="index.php">Recommandé</a></li>
-            <li><a href="recherche.php"> Recherche </a></li>
-        </ul>
-    </div>
-    <div id="form_donneesreco">
-        <form method="post" action="?">
-            <input id="nom" type="text" placeholder="Nom" required>
-            <input id="type" type ="text" placeholder="type" required>
-            <button type="submit">Ajouter</button>
-        </form>
-    </div>
-    <div id="bastableau">
-        <button id="pdf">Convertir en PDF</button>
-        <button id="print">Imprimer</button>
-    </div>
 </body>
 </html>
--- app/recherche.php
+++ app/recherche.php
@@ -1,18 +1,10 @@
-<?php
-
-?>
-
-<html>
+<html lang="FR">
 <head>
+    <title>Recherche</title>
     <link rel="stylesheet" href="style/style.css">
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script rel="script" src="script/header.js"></script>
 </head>
 <body>
-<div id="menu">
-    <ul id="onglets">
-        <li><a href="index.php"> Recommandé </a></li>
-        <li class="active"><a href="recherche.php"> Recherche </a></li>
-    </ul>
-</div>
 </body>
 </html>
