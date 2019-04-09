f56ab59e842b94f82abdcb3fa5312a5591f383e0
Benoit Joubert <37612886+benoit-joubert@users.noreply.github.com>
2019-04-09 11:48:42 +0200

    Ajout fichiers initiaux

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+
+<html>
+<head>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+</head>
+<body>
+<div id="menu">
+    <ul id="onglets">
+        <li><a href="index.php"> Recommandé </a></li>
+        <li class="active"><a href="test.php"> Recherche </a></li>
+    </ul>
+</div>
+</body>
+</html>
