855197b7e3cc65b943bb7716580373ada2d20f1c
Kévin-Falco <kevin.falco@etu.univ-amu.fr>
2019-04-10 16:12:41 +0200

    Formulaire d'ajout des recommandés

--- app/index.php
+++ app/index.php
@@ -7,18 +7,9 @@
     <script rel="script" src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <script rel="script" src="script/autocompletion.js"></script>
     <script rel="script" src="script/header.js"></script>
+    <script rel="script" src="script/completion_form.js"></script>
 </head>
 <body>
-    <div id="form_donneesreco">
-        <form method="post" action="?">
-            <input id="nom" type="text" placeholder="Nom" required>
-            <input id="type" type ="text" placeholder="type" required>
-            <button type="submit">Ajouter</button>
-        </form>
-    </div>
-    <div id="bastableau">
-        <button id="pdf">Convertir en PDF</button>
-        <button id="print">Imprimer</button>
-    </div>
+    <div id="completion_form"></div>
 </body>
 </html>
---
+++ app/script/completion_form.js
@@ -0,0 +1,86 @@
+/**
+ * Main function.
+ */
+(function () {
+    "use strict";
+    $(document).ready(function () {
+        createCompletionForm();
+    });
+}) ();
+
+function fillFileNumberInputScript(e) {
+    let input = String.fromCharCode(e.keyCode);
+    if (!/[a-zA-Z0-9]/.test(input)) return false;
+
+    let inputField = $('#file_number_input');
+    inputField.attr({
+        'value' : inputField.val(),
+        'placeholder' : ' '.repeat(inputField.val().length) + '*'.repeat(10 - inputField.val().length)
+    });
+}
+
+function createCompletionForm() {
+    let myFields = {
+        'Numero de dossier' : $('<input/>').attr({
+                                    'id' : 'file_number_input',
+                                    'type': 'text',
+                                    'maxlength' : 10,
+                                    'placeholder' : '**********',
+                                    'value' : '',
+                                    'onkeypress' : 'return fillFileNumberInputScript(event)',
+                                }),
+        'Type'              : $('<select/>')
+                                    .append($('<option/>').attr({'value' : 'arrete'}).html('Arrêté'))
+                                    .append($('<option/>').attr({'value' : 'courrier'}).html('Courrier'))
+                                    .append($('<option/>').attr({'value' : 'irrecevable'}).html('Irrecevable'))
+                                    .append($('<option/>').attr({'value' : 'notification'}).html('Notification des délais'))
+                                    .append($('<option/>').attr({'value' : 'pieces'}).html('Pièces Complémentaires'))
+                                    .append($('<option/>').attr({'value' : 'prolongation'}).html('Prolongation de délai'))
+                                    .append($('<option/>').attr({'value' : 'prorogation'}).html('Prorogation'))
+                                    .append($('<option/>').attr({'value' : 'rectificatif'}).html('Rectificatif'))
+                                    .append($('<option/>').attr({'value' : 'rejet'}).html('Rejet'))
+                                    .append($('<option/>').attr({'value' : 'report'}).html('Report de délai'))
+                                    .append($('<option/>').attr({'value' : 'retrait'}).html('Retrait'))
+                                    .append($('<option/>').attr({'value' : 'suspension'}).html('Suspension des délais')),
+        'Expediteur'        : $('<input/>').attr({'type': 'text'}),
+        'Imprimer'          : $('<select/>')
+                                    .append($('<option/>').attr({'value' : 'oui'}).html('Oui'))
+                                    .append($('<option/>').attr({'value' : 'non'}).html('Non')
+                                ),
+        'Date'              : $('<input/>').attr({'type': 'date'}),
+    };
+
+    let myForm =
+        $('<fieldset/>')
+            .append($('<legend/>')
+                .html('Ajout d\'un recommandé')
+            )
+            .append($('<div/>')
+                .addClass('table_container')
+                .append($('<table/>')
+                    .append($('<tr/>')
+                        .attr({
+                            'id' : 'labels'
+                        })
+                    )
+                    .append($('<tr/>')
+                        .attr({
+                            'id' : 'contents'
+                        })
+                    )
+                )
+            );
+
+    for (let fieldName in myFields) {
+        myForm.find('#labels')
+            .append($('<td/>')
+                .html(fieldName)
+            );
+        myForm.find('#contents')
+            .append($('<td/>')
+                .append(myFields[fieldName])
+            );
+    }
+
+    $('#completion_form').append(myForm);
+}
