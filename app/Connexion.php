7d99c67304b61cc471f76985313c9f422dea75e4
Kévin-Falco <kevin.falco@etu.univ-amu.fr>
2019-04-17 12:19:01 +0200

    Refractor

--- app/Connexion.php
+++ app/Connexion.php
@@ -6,7 +6,6 @@ class Connexion {
     private $password = 'test';
     private $host = 'sdiadem-cluster.intranet/SIGTEST';
     private $connected;
-    private $queryArray = [];
 
     public function __construct() {
 
@@ -17,54 +16,58 @@ class Connexion {
         );
     }
 
-    public function sqlRequest($select, $from, $where ='', $groupby='', $orderby='', $order='') {
+    public function sqlRequestView($select, $from, $where = '', $groupBy = '', $orderBy = '', $order = '') {
+
         // SELECT PART
         $sqlRequest = 'SELECT ';
         foreach ($select as $item){
             $sqlRequest .= $item. ', ';
         }
         $sqlRequest = substr($sqlRequest,0,-2);
+
         //FROM PART
         $sqlRequest .= ' FROM ';
         foreach ($from as $item){
             $sqlRequest .= $item. ', ';
         }
         $sqlRequest = substr($sqlRequest,0,-2);
+
         //WHERE PART
-        if ($where !='') {
+        if ($where != '') {
             $sqlRequest .= ' WHERE ';
             foreach ($where as $item) {
                 $sqlRequest .= $item . ' AND ';
             }
             $sqlRequest = substr($sqlRequest, 0, -5);
         }
+
         //GROUP BY PART
-        if ($groupby !='') {
+        if ($groupBy != '') {
             $sqlRequest .= ' GROUP BY ';
-            foreach ($groupby as $item) {
+            foreach ($groupBy as $item) {
                 $sqlRequest .= $item . ', ';
             }
             $sqlRequest = substr($sqlRequest, 0, -2);
         }
+
          //ORDER BY PART
-        if($orderby !='') {
+        if($orderBy != '') {
             $sqlRequest .= ' ORDER BY ';
-            foreach ($orderby as $item) {
+            foreach ($orderBy as $item) {
                 $sqlRequest .= $item . ', ';
             }
             $sqlRequest = substr($sqlRequest, 0, -2);
         }
+
         //ORDER PART
         if ($order != '') $sqlRequest .= ' ' . $order;
-
-
 
         return $sqlRequest;
     }
 
-    public function getElements($select, $from, $where ='', $groupby='', $orderby='', $order='') {
+    public function getElements($select, $from, $where ='', $groupBy='', $orderBy='', $order='') {
 
-        $stid = oci_parse($this->connected,$this->sqlRequest($select, $from, $where, $groupby, $orderby, $order));
+        $stid = oci_parse($this->connected,$this->sqlRequestView($select, $from, $where, $groupBy, $orderBy, $order));
         oci_execute($stid);
 
         $result = array();
--- app/index.php
+++ app/index.php
@@ -2,9 +2,9 @@
     require 'Connexion.php';
 
     $db = new Connexion();
-    $listFileType = $db->getElements(array('TYPDOS'),array('TYPDOS'));
-    $listRemarks = $db->getElements(array('REMARQUE'),array('REMARQUES'));
-    $listRegistered = $db->getElements(array('TYPE_ENVOI', 'DOSSIER', 'DEMANDEUR', 'ADRESSE_1', 'ADRESSE_2', 'ADRESSE_3'),array('T_COMPLETE'),array('ROWNUM <= 5'));
+    $listFileType = $db->getElements(array('TYPDOS'), array('TYPDOS'));
+    $listRemarks = $db->getElements(array('REMARQUE'), array('REMARQUES'));
+    $listRegistered = $db->getElements(array('TYPE_ENVOI', 'DOSSIER', 'DEMANDEUR', 'ADRESSE_1', 'ADRESSE_2', 'ADRESSE_3'), array('T_COMPLETE'), array('ROWNUM <= 5'));
 ?>
 <html lang="fr-FR">
 <head>
